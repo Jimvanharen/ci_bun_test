5dd6de6bdd737b8a6b693448e2f86845a950f832
Jim van Haren <jimdedrummer99@gmail.com>
2024-04-18 14:01:45 +0200

    x

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'bun:test';
+
+
+
+const __main__ = () => {
+    console.log('Hello, World!');
+}
+
+
+__main__();
---
+++ src/test/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "bun:test";
+
+
+test("Hello, World!", () => {
+    expect("Hello, World!").toBe("Hello, World!");
+});
